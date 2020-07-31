0901f2535b51d4e29ab2ba1314252a151aabe15f
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-31 18:00:02 +0300

    Update POST method

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
-from flask.globals import request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # //// is absolute path
 db = SQLAlchemy(app)
 
@@ -21,11 +21,18 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
-    else:
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
-    return render_template('index.html')
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
