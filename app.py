a4cad82e59c58957088dd128fb0fb35237bac41c
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-31 17:28:33 +0300

    Catch POST request

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,8 +18,13 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Task {self.id}>'
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
     return render_template('index.html')
 
 if __name__ == '__main__':
